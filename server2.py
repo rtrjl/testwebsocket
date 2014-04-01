197364072c0f0c6f34f5f0a469f17a7bb7e333c8
rtrjl <rt@zone42.fr>
2014-04-01 17:15:39 +0200

    pouet

--- server2.py
+++ server2.py
@@ -1,32 +1,84 @@
 import asyncio
-from http.server import BaseHTTPRequestHandler
+import base64
+from hashlib import sha1
 from io import StringIO
 
 
 incomingQueue = asyncio.Queue()
 
 
+class WSException(Exception):
+    # Subclasses that define an __init__ must call Exception.__init__
+    # or define self.args.  Otherwise, str() will fail.
+    pass
+
+
 class WebSocketRequest():
-    MAXHEADERS = 4096
+    MAX_HEADERS = 4096
+    WS_KEY = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+    http_method = ""
+    http_version = ""
+    http_path = ""
+    headers = {}
 
     def __init__(self, request_data):
-       
         self.parse_request(request_data)
-    
+
     def handshake(self):
-        #wskey = self.headers.get('Sec-WebSocket-Key')
-        print("coucou")
-        
+        ws_key = ""
+        ws_version = 0
+        try:
+            ws_key = self.headers['Sec-WebSocket-Key']
+        except KeyError:
+            raise WSException("WebSocket key missing")
+        ws_key = base64.b64decode(ws_key)
+        if len(ws_key) != 16:
+            raise WSException("Websocket key lenght is != 16")
+        try:
+            ws_version = int(self.headers['Sec-WebSocket-Version'])
+        except ValueError:
+            raise WSException("Websocket version must be an integer")
+        if ws_version != 13:
+            raise WSException("Websocket version must be 13")
+
+        handshake_headers = [
+            ('Upgrade', 'websocket'),
+            ('Connection', 'Upgrade'),
+        ]
+        print(type(ws_key))  # gné ?
+        ws_accept_key = base64.b64encode(sha1(ws_key
+                                              + self.WS_KEY).digest())
+        handshake_headers['Sec-WebSocket-Accept'] = ws_accept_key
+        response = "HTTP/1.1 101 Switching Protocols"
+
+        for key in handshake_headers.keys():
+            response += key + ": " + handshake_headers[key]
+
+        return response.encode()
+
     def send_error(self, code, message):
         self.error_code = code
         self.error_message = message
 
     def parse_request(self, request_data):
-        headers = {}
-        if len(request_data) > 4096:
-            raise HTTPException("Header request too long")
-        request_str = request_data.decode()
-        #for line in request_str
+        if len(request_data) > self.MAX_HEADERS:
+            raise WSException("Request too long for me")
+
+        request_str = StringIO(request_data.decode())
+        try:
+            self.http_method, self.http_path, self.http_version \
+                = request_str.readline().split(" ")
+        except:
+            raise WSException("Malformed request line")
+
+        if self.http_method != "GET":
+            raise WSException("HTTP method must be GET")
+
+        for line in request_str.readlines():
+            if line in ("\r\n", "\n", ""):
+                break
+            key, value = line.split(":", 1)
+            self.headers[key] = value.strip()
 
 
 class PicoChatProtocol(asyncio.Protocol):
@@ -41,8 +93,8 @@ class PicoChatProtocol(asyncio.Protocol):
     def data_received(self, data):
         if self.handshake_done is False:
             req = WebSocketRequest(data)
-            req.handshake()
-            self.transport.write(data)
+
+            self.transport.write(req.handshake())
 
         # close the socket
         self.transport.close()
@@ -60,9 +112,3 @@ except KeyboardInterrupt:
 finally:
     server.close()
     loop.close()
-
-
-
-
-
-

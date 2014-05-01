07c32d989d925a9e623c6047bf4708214aed3e36
rtrjl <rt@zone42.fr>
2014-05-01 21:31:11 +0200

    pouet du 1er mai

--- tester.py
+++ tester.py
@@ -1,16 +1,9 @@
 import unittest
-import hashcash
 from hashlib import sha256
 from hashcash import test_bytes, test_byte
 
 
 class TestFonctions(unittest.TestCase):
-
-    def test_test_bytes(self):
-        self.assertEqual(test_bytes(sha256("426479724".encode("utf-8")).digest(), 32), True)
-        self.assertEqual(test_bytes(sha256("665782".encode("utf-8")).digest(), 22), True)
-        self.assertEqual(test_bytes(sha256("886".encode("utf-8")).digest(), 12), True)
-        self.assertEqual(test_bytes(sha256("8".encode("utf-8")).digest(), 2), True)
 
     def test_test_byte(self):
         self.assertEqual(test_byte(0b00000000, 8), True)
@@ -22,6 +15,14 @@ class TestFonctions(unittest.TestCase):
         self.assertEqual(test_byte(0b00111111, 2), True)
         self.assertEqual(test_byte(0b01111111, 1), True)
         self.assertEqual(test_byte(0b11111111, 0), True)
+
+    def test_test_bytes(self):
+        self.assertEqual(test_bytes(sha256("426479724".encode("utf-8")).digest(), 32), True)
+        self.assertEqual(test_bytes(sha256("665782".encode("utf-8")).digest(), 22), True)
+        self.assertEqual(test_bytes(sha256("886".encode("utf-8")).digest(), 12), True)
+        self.assertEqual(test_bytes(sha256("8".encode("utf-8")).digest(), 2), True)
+
+
 
 if __name__ == '__main__':
     unittest.main()
